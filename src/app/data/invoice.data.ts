b3f59ef2073e4bef4802cafa1ed54c12d26871e6
Exe <exelcarrizo@gmail.com>
2024-06-25 21:13:51 -0300

    all views separated by components

---
+++ src/app/data/invoice.data.ts
@@ -0,0 +1,39 @@
+export const invoiceData = {
+
+  id: 1,
+  name: 'Componentes de PC',
+  client: {
+    name: 'Exequiel',
+    lastname: 'Carrizo',
+    address: {
+      country: 'USA',
+      city: 'Los Angeles',
+      street: 'Groove Street',
+      number: 33,
+    }
+  },
+  company: {
+    name: 'New Age',
+    fiscalNumber: 153215,
+  },
+  items: [
+    {
+      id: 1,
+      product: 'CPU Ryzen 7',
+      price: 699,
+      quantity: 1
+    },
+    {
+      id: 2,
+      product: 'Red Dragon Teclado Mecanico',
+      price: 199,
+      quantity: 2
+    },
+    {
+      id: 3,
+      product: 'Monitor Samsung 24 Pulgadas',
+      price: 799,
+      quantity: 3
+    },
+  ]
+}
